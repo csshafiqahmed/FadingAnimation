fe7592f97b17b2098dfa0c1cdeb60e6496f03b10
Shafiq Ahmed <csshafiqahmed@gmail.com>
2020-10-14 01:45:25 +0500

    comments added

--- app/src/main/java/com/codestown/fadinganimation/MainActivity.java
+++ app/src/main/java/com/codestown/fadinganimation/MainActivity.java
@@ -1,18 +1,21 @@
 package com.codestown.fadinganimation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+//fading animation project
+
     boolean baby1showing = true;
-    public void fade(View view){
+
+    public void fade(View view) {
 
         ImageView baby1ImageView = findViewById(R.id.baby1ImageView);
         ImageView baby2ImageView = findViewById(R.id.baby2ImageView);
-        if (baby1showing){
+        if (baby1showing) {
             baby1showing = false;
             baby1ImageView.animate().alpha(0).setDuration(2000);
             baby2ImageView.animate().alpha(1).setDuration(2000);
